2d4818f7024cd2da6350add71688e3e85110576c
Lavanya25082001 <ammulavanya0125@gmail.com>
2024-03-08 11:11:20 +0530

    booking

---
+++ features/stepdefinitions/steps.js
@@ -0,0 +1,66 @@
+const { Given, When, Then } = require("@wdio/cucumber-framework");
+const { expect, $ } = require("@wdio/globals");
+const chai = require("chai");
+const LoginPage = require("../pageobjects/login.page");
+const SearchPage=require("../pageobjects/search.page");
+const ProfilePage=require("../pageobjects/profile.page");
+const flightPage=require("../pageobjects/flights.page")
+require('dotenv').config();
+const data=require("../../Data/data.json")
+
+Given(/^I am navigating login page of Booking.com application$/, async() => {
+	await LoginPage.Gotologinpage();
+});
+
+When(/^I login with <username> and <password>$/, async() => {
+  await LoginPage.loginfunction(
+    process.env.valid_username,
+    process.env.valid_password,
+  );
+});
+Then(/^I can see landing page$/, async() => {
+  await LoginPage.verifyingLandingPage();
+});
+
+
+When(/^I click on search and navigate to search page$/, async() => {
+	await SearchPage.navigateToSearchPage();
+});
+
+
+Then(/^I can search for stay$/, async() => {
+	await SearchPage.enterDestination();
+  await SearchPage.selectDates();	
+  await SearchPage.enterRoomDetails(); 
+});
+
+
+Then(/^I can sort the hotel list and add to my wishlist$/, async() => {
+  await SearchPage.selectHotelHighRange();	
+ 
+});
+
+Then(/^I can verify WishList$/, async() => {
+	await SearchPage.Wishlist();
+});
+
+
+Then(/^I can perform profile actions$/, async() => {
+await ProfilePage.GotoProfilePage();
+});
+
+
+Then(/^I can search for flights$/, async() => {
+	await flightPage.GotoFlights();
+});
+
+When(/^I login with invalid <username> and <password>$/, async() => {
+  await LoginPage.loginIvalidCredentials(
+    process.env.Invalid_username,
+    process.env.Invalid_password,
+    process.env.Incorrect_username,
+    process.env.Incorrect_password,
+    process.env.valid_username,
+    process.env.valid_password
+  );
+});
